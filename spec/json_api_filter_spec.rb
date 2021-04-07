d0b4acaa007d1d635895c66b480f59ccefda81d1
blaked <becker.dorian@gmail.com>
2021-04-07 10:02:09 +0200

    add dummy app and tests

--- spec/json_api_filter_spec.rb
+++ spec/json_api_filter_spec.rb
@@ -8,21 +8,104 @@ RSpec.describe JsonApiFilter do
   end
   after { Object.send :remove_const, :FakesController }
   let(:object) { FakesController.new }
-  let(:params) do
-    {
-      filter:
-        {
-          id: "1,2",
-          author: "12",
-          book: "14"
-        }
-    }
-  end
-  let(:output) { {id: %w[1 2], author: %w[12]} }
+  klass_examples = [
+      {
+        name: "JsonApiFilter::FieldFilters::Matcher",
+        examples: [
+          {
+            name: 'direct id',
+            params: {
+              filter: { id: "1,2" }
+            },
+            request: User.where(id: [1,2])
+          }
+       ],
+      },
+      {
+        name: "JsonApiFilter::FieldFilters::Compare",
+        examples: [
+          {
+            name: 'eq id',
+            params: {
+              filter: { id: {eq: 1} }
+            },
+            request: User.where("id = 1")
+          },
+          {
+            name: 'not eq id',
+            params: {
+              filter: { id: {ne: 1} }
+            },
+            request: User.where("id != 1")
+          },
+          {
+            name: 'gt id',
+            params: {
+              filter: { id: {gt: 1} }
+            },
+            request: User.where("id > 1")
+          },
+          {
+            name: 'ge id',
+            params: {
+              filter: { id: {ge: 1} }
+            },
+            request: User.where("id >= 1")
+          },
+          {
+            name: 'lt id',
+            params: {
+              filter: { id: {lt: 1} }
+            },
+            request: User.where("id < 1")
+          },
+          {
+            name: 'le id',
+            params: {
+              filter: { id: {le: 1} }
+            },
+            request: User.where("id <= 1")
+          },
+          {
+            name: 'le id and gt id',
+            params: {
+              filter: {
+                id: {
+                  gt: 1,
+                  lt: 3,
+                },
+              }
+            },
+            request: User.where("id > 1").where("id < 3")
+          },
+          {
+            name: 'le id and eq name',
+            params: {
+              filter: {
+                id: {le: 1},
+                name: {eq: "foo"}
+              }
+            },
+            request: User.where("id <= 1").where("name = 'foo'")
+          }
+       ]
+      }
+    ]
 
 
-  describe 'filter_by_attr' do
-    it { expect(object.attr_filter(params)).to eq(output) }
+  klass_examples.each do |klass|
+  
+    describe klass[:name] do
+    
+      klass[:examples].each do |test_case|
+        tc = test_case.with_indifferent_access
+        
+        it "Filter by #{tc['name']}" do
+          expect(object.attr_filter(User, tc['params'])).to eq(tc['request'])
+        end
+      end
+    end
+    
   end
 
 end
