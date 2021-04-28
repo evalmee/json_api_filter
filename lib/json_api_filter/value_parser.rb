916cef6a6322262b25fbaf3e6ed6ec434377fce0
blaked <becker.dorian@gmail.com>
2021-04-28 17:16:16 +0200

    add documentation

--- lib/json_api_filter/value_parser.rb
+++ lib/json_api_filter/value_parser.rb
@@ -8,10 +8,20 @@ module JsonApiFilter
       @value = value
     end
     
+    # Transform string of coma separated values to Array
+    #
+    # @example
+    #   "1,2,3" #=> [1,2,3]
+    #
+    # @param [String] value
+    # @return [Array]
     def self.parse(value)
       new(value).parse
     end
     
+    private
+    
+    # @private
     def parse
       return [value] unless value.include?(",")
       value.split(',')
